10f6f5217efa23f1a3eeb4e6ff7344628ba0a061
Vadim Samodurov <75vadimalena96@gmail.com>
2025-06-23 15:05:14 +0300

    Update note.ts

--- src/types/note.ts
+++ src/types/note.ts
@@ -4,8 +4,8 @@ export interface Note {
     id: number;
     title: string;
     content: string;
-    createdAt?: string;
-    updatedAt?: string;
+    createdAt: string;
+    updatedAt: string;
     tag: TagType;
 }
 
